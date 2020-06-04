39a87a30ab10ba9ccc014248de1e710ccffe737b
Lillian Blair <lilliand_2000@yahoo.com>
2020-06-03 21:16:33 -0500

    Started adding Firebase to Practice SPA

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAbKG63vnBFUtnfgA6719BdK7w4hB_BHB8",
+  authDomain: "apr-2020-practice-spa-5d459.firebaseapp.com",
+  databaseURL: "https://apr-2020-practice-spa-5d459.firebaseio.com",
+  projectId: "apr-2020-practice-spa-5d459",
+  storageBucket: "apr-2020-practice-spa-5d459.appspot.com",
+  messagingSenderId: "7870233303",
+  appId: "1:7870233303:web:f04010a543e900bb0dce92"
+};
